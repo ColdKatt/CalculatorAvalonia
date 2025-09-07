d69acc6c2b5d1484351508d394f693afd20dc222
ColdKatt <ColdKatt@users.noreply.github.com>
2025-09-08 03:39:53 +0500

    feat: added converter for 0-9 buttons

---
+++ Converters/DigitHotKeyConverter.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Input;
+using System;
+using System.Globalization;
+
+namespace CalculatorAvalonia.Converters
+{
+    public class DigitHotKeyConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return KeyGesture.Parse("D" + value?.ToString());
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            var notification = new Avalonia.Data.BindingNotification(value);
+            notification.AddError(new NotImplementedException(), Avalonia.Data.BindingErrorType.Error);
+            return notification.Error;
+        }
+    }
+}
